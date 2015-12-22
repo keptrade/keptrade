df8cbbfe4e16b505f02327565525a7dfac162dbc
xuxw76 <xuxw76@yahoo.com>
2015-12-21 20:48:14 -0500

    make it better

--- public/js/login_ctrl.js
+++ public/js/login_ctrl.js
@@ -1,19 +1,16 @@
 goog.provide('gn311.LoginCtrl');
 
-gn311.LoginCtrl = function($scope, $location, loginService) {
+gn311.LoginCtrl = function($location, loginService) {
     this.location_ = $location;
     this.auth_ = loginService.getAuth();
-    $scope.loginAsFacebook = this.loginAsFacebook.bind(this);
-    $scope.loginAsGoogle = this.loginAsGoogle.bind(this);
-    $scope.loginAsGuest = this.loginAsGuest.bind(this);
 };
 
 gn311.LoginCtrl.prototype.loginAsFacebook = function() {
-  this.auth_.$login('facebook');
+    this.auth_.$authWithOAuthPopup("facebook");
 };
 
 gn311.LoginCtrl.prototype.loginAsGoogle = function() {
-  this.auth_.$login('google');
+  this.auth_.$authWithOAuthPopup("google");
 };
 
 gn311.LoginCtrl.prototype.loginAsGuest = function() {
--- public/js/services/firebase_services.js
+++ public/js/services/firebase_services.js
@@ -1,10 +1,10 @@
 //services come here
 var fireBaseServices = angular.module('fireBaseServices', [], function($provide){
-    $provide.factory("loginService", function($firebase, $firebaseSimpleLogin) {
+    $provide.factory("loginService", function($firebase, $firebaseAuth) {
         return {
             getAuth :  function(){
                 var ref = new Firebase("https://resplendent-torch-4949.firebaseio.com");
-                return $firebaseSimpleLogin(ref);
+                return $firebaseAuth(ref);
             }
         };
     });
